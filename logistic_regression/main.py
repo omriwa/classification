fb206013de2a8130e15f7e632e0c566f8a584935
omri wallach <omriwallach1992@gmail.com>
2021-01-04 16:54:51 +0100

    accuracy score logistic regression

--- logistic_regression/main.py
+++ logistic_regression/main.py
@@ -23,7 +23,8 @@ classifier = LogisticRegression()
 classifier.fit(X_train, y_train)
 
 # confusion matrix
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix,accuracy_score
 
 cm = confusion_matrix(y_test, classifier.predict(X_test))
 print(cm)
+print(accuracy_score(y_test, classifier.predict(X_test)))
