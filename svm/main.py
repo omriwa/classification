26558fbbcc618dcfe1d4da1d74e9ad53dada7acd
omri wallach <omriwallach1992@gmail.com>
2021-01-05 10:15:14 +0100

    change svm model params

--- svm/main.py
+++ svm/main.py
@@ -24,7 +24,7 @@ X_test = sc.transform(X_test)
 
 # Training the model
 from sklearn.svm import SVC as model
-classifier = model()
+classifier = model(kernel="linear",random_state=0)
 classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
