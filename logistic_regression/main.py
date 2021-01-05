183d6455f81b8d4933000517287255ac7b00db13
omri wallach <omriwallach1992@gmail.com>
2021-01-05 09:00:40 +0100

    training knn model

--- logistic_regression/main.py
+++ logistic_regression/main.py
@@ -13,26 +13,17 @@ y = dataset.iloc[:, -1].values
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
-print(X_train)
-print(y_train)
-print(X_test)
-print(y_test)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
-print(X_train)
-print(X_test)
 
 # Training the Logistic Regression model on the Training set
-from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state = 0)
+from sklearn.linear_model import LogisticRegression as model
+classifier = model(n_neighbors= 10)
 classifier.fit(X_train, y_train)
-
-# Predicting a new result
-print(classifier.predict(sc.transform([[30,87000]])))
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
